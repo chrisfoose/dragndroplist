e3944e508ccc3a52bd65461572684ac47de5bae4
chrisfoose <47850167+chrisfoose@users.noreply.github.com>
2023-03-08 17:24:26 -1000

    drag over worksgit add .!

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 const draggables = document.querySelectorAll('.draggable')
 const containers  =  document.querySelectorAll('.container')
 
-draggables.forEach(draggables => {
+draggables.forEach(draggable => {
     draggable.addEventListener('dragstart', () => {
-        draggables.classList.add('dragging')
+        draggable.classList.add('dragging')
     })
 
     draggable.addEventListener('dragend', () => {
         draggable.classList.remove('dragging')
     })
 
-containers.forEach(container = > {
+containers.forEach(container => {
     container.addEventListener('dragover', () => {
     console.log('drag over')
     })
